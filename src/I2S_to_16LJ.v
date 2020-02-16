2cb2e066ef0eb564167f0a451993e207cfa4d97c
torinomiya <42882686+torinomiya@users.noreply.github.com>
2020-02-16 23:13:15 +0900

    Delete useless comments

--- src/I2S_to_16LJ.v
+++ src/I2S_to_16LJ.v
@@ -1,23 +1,23 @@
-//I2SをBCK = 32fsの16LJに変換
+//I2Sを16LJに変換
 module I2S_to_16LJ(bck, data, lrck, bck_out, data_out, lrck_out);
 
-	input bck; //-- bck
-	input data; // --data
-	input lrck; //-- Frame sync (asserted for channel A, negated for B)
+	input bck;
+	input data;
+	input lrck;
 	output bck_out;
 	output data_out;
 	output lrck_out;
 
-	reg data_out = 0; 			//bck立下り基準なので、DATA出力は0初期化しておく
-	reg[4:0] data_counter = 0 ; //元のI2Sにおける　何bitめのDATAか
+	reg data_out = 0; 			
+	reg[4:0] data_counter = 0 ; //元のI2Sにおける何bitめのDATAか
 	reg[31:0] data_fifo = 0; 	//元のI2S DATA (32bit) を 1sample分格納する
 
-	//LRCK を 元々の2周期分遅らせる
+	//LRCK を 元々のBCK2周期分遅らせる
 	wire lrck_32LJ;
 	delay_1BCK I2S_to_32LJ (lrck, bck, lrck_32LJ);
 	delay_1BCK delay (lrck_32LJ, bck, lrck_out);	
 	
-	//BCKを1/2分周
+	//BCKを2分周
 	//lrck_changed はないと、BCKのタイミングが狂うことがある
 	reg lrck_changed;
 	half_freq half_freq_ins (bck, lrck_changed, bck_out);
@@ -26,7 +26,6 @@ module I2S_to_16LJ(bck, data, lrck, bck_out, data_out, lrck_out);
 	reg lrck_before = 0;
 	always @ (posedge bck)
 	begin
-		//lrckの切り替わりを検出、読んでるbitのインデックスを0にする
 		if(lrck_before != lrck_32LJ)
 		begin
 			data_counter = 0;
