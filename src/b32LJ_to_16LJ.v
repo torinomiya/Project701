09410bc9ebfcd736ac8c83c3c4ae7a5533c73b6d
torinomiya <42882686+torinomiya@users.noreply.github.com>
2020-02-21 00:20:18 +0900

    すべてNonBlocking代入にした。

--- src/b32LJ_to_16LJ.v
+++ src/b32LJ_to_16LJ.v
@@ -10,8 +10,8 @@ module b32LJ_to_16LJ(bck, data, lrck_32LJ, bck_out, data_out);
 	reg[4:0] data_counter = 0 ; //元のI2Sにおける　何bitめのDATAか
 	reg[31:0] data_fifo = 0; 	//元のI2S DATA (32bit) を 1sample分格納する
 
-	//BCKを1/2分周
-	//lrck_changed はないと、BCKのタイミングが狂うことがある
+	//BCKを2分周
+	//元のBCKに同期して、lrck_changedでリセットをかける
 	reg lrck_changed;
 	half_freq half_freq_ins (bck, lrck_changed, bck_out);
 	
@@ -22,16 +22,17 @@ module b32LJ_to_16LJ(bck, data, lrck_32LJ, bck_out, data_out);
 		//lrckの切り替わりを検出、読んでるbitのインデックスを0にする
 		if(lrck_before != lrck_32LJ)
 		begin
-			data_counter = 0;
+			data_counter <= 0;
+			data_fifo[0] <= data;
 			lrck_changed <= 1;
 		end
 		else
 		begin
+			data_counter <= data_counter + 1;
+			data_fifo[data_counter + 1] <= data;
 			lrck_changed <= 0;
 		end
-		data_fifo[data_counter] = data;
-		lrck_before = lrck_32LJ;
-		data_counter = data_counter + 1;
+		lrck_before <= lrck_32LJ;
 	end
 
 	always @ (negedge bck_out)
